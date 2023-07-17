bfb393a07c0a56a6aba10c6f7f87b72cda073d7b
FultonGreiner <65039828+FultonGreiner@users.noreply.github.com>
2023-07-17 12:03:24 -0500

    implemented minimal kernel and boot sequence targetting ARM CortexA15

---
+++ kernel/_start.s
@@ -0,0 +1,16 @@
+// declare `_start` globally for access & linking
+.global _start
+
+// entry point (code execution begins here)
+_start:
+    // set stack pointer to top address of stack memory
+    ldr sp, =STACK_TOP
+    // save return address, branch to `start` subroutine
+    bl start
+
+1:
+    // unconditional branch to `1` label
+    b 1b
+
+// calculate size of `_start` label
+.size _start, . - _start
