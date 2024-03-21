bcc670f74358f42f8e5bbff635ee485ba3fddab7
FultonGreiner <65039828+FultonGreiner@users.noreply.github.com>
2024-03-20 21:34:30 -0700

    Restructuring

--- source/kernel/start.c
+++ source/kernel/start.c
@@ -1,30 +1,9 @@
-/*
- *  uart-zynq-main.c
- *
- *  Simple test for uart-zynq driver
- *
- *  2017.05.23 Created
- *
- */
-
 #include <string.h>
+
+// #include <printf.h>
 
 #include "printf.h"
 #include "uart-zynq.h"
-
-/*---------------------------------------------------------------------------
-** Entry conditions:    None.
-**
-** Exit conditions:     None.
-**
-** Purpose:             Entry point to the application.
-**
-** Parameters:          Void.
-**
-** Returns:             Void.
-**
-** Notes:
-*/
 
 #define RX_BUFFER_SIZE 100
 static char rx_buffer[RX_BUFFER_SIZE] = {0};
@@ -35,26 +14,24 @@ static char status_buffer[STATUS_BUFFER_SIZE] = {0};
 
 #define CARRIAGE_RETURN (0xDUL)
 #define BACKSPACE       (0x8UL)
+#define NEWLINE         (0xAUL)
 
-int main()
+#define KERNEL_PROMPT   ("> ")
+
+int main(void)
 {
     char ch[6] = "R:  ";
     float f = 3.14159;
     uart_initialise();
 
-    printf("Hello, world!\r\n");
+    printf("\nHello, world!\r\n");
 
     while (1)
     {
-        /*
-        * Read the character, and then
-        * print out with formatter.
-        */
-
         memset((void*)rx_buffer, 0, RX_BUFFER_SIZE);
         memset((void*)status_buffer, 0, STATUS_BUFFER_SIZE);
 
-        printf("\r\n> ");
+        printf(KERNEL_PROMPT);
 
         char c = 0;
         rx_buffer_index = 0;
@@ -63,13 +40,10 @@ int main()
         do
         {
             rx_buffer[i] = read_uart_char(UART1_BASE);
-            // if ( BACKSPACE == rx_buffer[i] )
-            // {
-            //     printf("BACKSPACE\n\r");
-            // }
             write_uart_char(UART1_BASE, rx_buffer[i]);
-            i++;
         } while ( CARRIAGE_RETURN != rx_buffer[i++] );
+
+        printf("\r\n");
     }
 
     return 0;
