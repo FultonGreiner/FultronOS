e2aa6dcd06f434814b37b949690cc036ecfd9761
FultonGreiner <65039828+FultonGreiner@users.noreply.github.com>
2024-03-21 23:33:53 -0700

    Restructuring

--- cortex-a9.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Rebuild the project
-rm -rf Debug
-cmake -DCMAKE_TOOLCHAIN_FILE=cmake/arm-none-eabi-gcc.cmake  -S ./ -B Debug -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
-make -C Debug VERBOSE=1
--- emulate.sh
+++ emulate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/bin/bash -c "/Applications/Renode.app/Contents/MacOS/macos_run.command renode-config.resc"
+/bin/bash -c "/Applications/Renode.app/Contents/MacOS/macos_run.command tools/renode/renode-config.resc"
